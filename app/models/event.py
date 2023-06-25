2822203902f47cb80439f4832aff0a2d89dc3bad
collin-smith23 <collinmk@live.com>
2023-06-25 01:09:59 -0600

    added model for event

---
+++ app/models/event.py
@@ -0,0 +1,45 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from .user import User
+from datetime import datetime
+
+
+
+class Event(db.Model):
+    __tablename__ = 'events'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String(255), nullable=False)
+    event_date = db.Column(db.Date, nullable=False)
+    event_time = db.Column(db.Time, nullable=False)
+    event_details = db.Column(db.String(2500))
+    estimated_cost = db.Column(db.Decimal)
+    predicted_revenue = db.Column(db.Decimal)
+    private = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    owner = db.relationship('User', backref=db.backref('events'))
+
+    def is_future_date(self):
+        current_date = datetime.now().date()
+        return self.event_date >= current_date
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'event_name': self.event_name,
+            'event_date': self.event_date.strftime('%m-%d-%Y'),
+            'event_time': self.event_time.strftime('%H:%M'),
+            'event_details': self.event_details,
+            'estimated_cost': self.estimated_cost,
+            'predicted_revenue': self.predicted_revenue,
+            'private': self.private,
+            'owner_id': self.owner_id,
+            'owner_name': self.owner.name,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
