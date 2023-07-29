ea57f767b3a37b32300a6d65c486bdb126c198a2
collin-smith23 <collinmk@live.com>
2023-07-29 14:20:02 -0600

    working on state

--- react-app/src/store/finance.js
+++ react-app/src/store/finance.js
@@ -2,6 +2,45 @@ const GET_FINANCE = 'finance/ADD_FINANCE';
 const GET_FINANCES = 'finance/GET_FINANCES';
 const REMOVE_FINANCE = 'finance/REMOVE_FINANCE';
 
+const getFinances = (finances) => ({
+    type: GET_FINANCES,
+    payload: finances
+})
+const getFinance = (finance) => ({
+    type: GET_FINANCE,
+    payload: finance
+})
+const removeFinance = (finance) => ({
+    type: GET_FINANCE,
+    payload: finance
+})
+
+export const getUserFinances = () => async (dispatch) => {
+    const res = await fetch(`api/finance/`);
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(GET_FINANCES(data));
+        return data
+    } else {
+        const error = await res.json()
+        return error
+    }
+}
+
+export const getFinanceRecord = (financeId) => async (dispatch) => {
+    const res = await fetch(`/api/finance/${financeId}`);
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(getFinance(data));
+        return data
+    } else {
+        const error = await res.json();
+        return error
+    }
+};
+
+
+
 const initialState = { finance: {}, finances: []};
 export default function finance(state = initialState, action) {
     let newState = Object.assign(state)
