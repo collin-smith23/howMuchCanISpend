0264c3d3a4a7462656b3029f2bd428f08a67747a
collin-smith23 <collinmk@live.com>
2023-08-09 22:17:24 -0600

    fixed some css styling for homepage

--- react-app/src/components/finance/contextMenu.js
+++ react-app/src/components/finance/contextMenu.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContextMenu({ x, y, onClose, onDelete }) {
+    const [hoveredOption, setHoveredOption] = useState(null);
+
+    const handleMouseEnter = (option) => {
+        setHoveredOption(option);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredOption(null);
+    };
+
     const handleClick = (action) => {
         if (action === "delete") {
             onDelete();
@@ -11,8 +21,22 @@ function ContextMenu({ x, y, onClose, onDelete }) {
 
     return (
         <div className="context-menu" style={{ top: y, left: x }}>
-            <div onClick={() => handleClick("delete")}>Delete</div>
-            <div onClick={() => handleClick("cancel")}>Cancel</div>
+            <div
+                className={`context-menu-item ${hoveredOption === "delete" ? "hovered" : ""}`}
+                onClick={() => handleClick("delete")}
+                onMouseEnter={() => handleMouseEnter("delete")}
+                onMouseLeave={handleMouseLeave}
+            >
+                Delete
+            </div>
+            <div
+                className={`context-menu-item ${hoveredOption === "cancel" ? "hovered" : ""}`}
+                onClick={() => handleClick("cancel")}
+                onMouseEnter={() => handleMouseEnter("cancel")}
+                onMouseLeave={handleMouseLeave}
+            >
+                Cancel
+            </div>
         </div>
     );
 }
--- react-app/src/components/finance/finance.js
+++ react-app/src/components/finance/finance.js
@@ -32,8 +32,6 @@ function Finances() {
         e.preventDefault();
         setContextMenuPos({ x: e.clientX, y: e.clientY });
         setShowContextMenu(true);
-        // Store the selected financeId in state or a ref to use in the delete function
-        // For example, you can use a ref like this:
         setSelectedFinanceId(financeId);
     };
 
@@ -42,41 +40,38 @@ function Finances() {
     };
 
     const handleDelete = () => {
-        // Use the stored financeId to delete the finance record
-        // For example, if you use the ref selectedFinanceId:
-        dispatch(financeActions.deleteTransaction(selectedFinanceId));
+        const confirmed = window.confirm("Are you sure you want to delete this record?");
+        if (confirmed) {
+            dispatch(financeActions.deleteTransaction(selectedFinanceId));
+        }
         setShowContextMenu(false);
     };
 
     return finances ? (
         <div className="finance-contain">
-            <h1>Finances</h1>
-            <div className="finance-box" >
+            <h2 className="title finance">Finances</h2>
+            <div className="finance-box">
+                <div className="category-title-box">
+                    <div className="category-title">Transaction Details</div>
+                    <div className="category-title">Transaction Date</div>
+                    <div className="category-title">Amount</div>
+                </div>
 
                 {Object.values(finances).map((finance, index) => {
                     const { amount, transaction_type } = finance;
-                    let result = null;
-
-                    if (transaction_type === "Expense") {
-                        result = `-$${amount}`;
-                    } else if (transaction_type === "Income") {
-                        result = `+$${amount}`;
-                    }
+                    const result = transaction_type === "Expense" ? `-$${amount}` : `+$${amount}`;
 
                     return (
-                        <>
-                            <div key={index} className="finance-item" onContextMenu={(e) => handleContextMenu(e, finance.id)}>
-                                <div>{finance.transaction_details}</div>
-                                <div>{finance.transaction_date}</div>
-                                <div>{result}</div>
-                            </div>
-                        </>
+                        <div key={index} className="finance-item" onContextMenu={(e) => handleContextMenu(e, finance.id)}>
+                            <div className="category">{finance.transaction_details}</div>
+                            <div className="category">{finance.transaction_date}</div>
+                            <div className="category">{result}</div>
+                        </div>
                     );
                 })}
+
                 <div className="create-finance-button">
-                    <button
-                        onClick={(e) => history.push('/finance/new')}
-                    > Create a new record </button>
+                    <button onClick={(e) => history.push('/finance/new')}> Create a new record </button>
                 </div>
             </div>
             {showContextMenu && (
@@ -92,9 +87,7 @@ function Finances() {
         <div className="no-finance-box">
             <div className="no-finance">No Finances to track</div>
             <div className="create-finance-button">
-                <button
-                    onClick={(e) => history.push('/finance/new')}
-                > Create a new record </button>
+                <button onClick={(e) => history.push('/finance/new')}> Create a new record </button>
             </div>
         </div>
     );
