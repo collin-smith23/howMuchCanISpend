b9db025d76a9223d195e2f564ad7f0ad9bbd30a9
collin-smith23 <collinmk@live.com>
2023-08-15 17:49:41 -0600

    dark mode not working yet

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import './Navigation.css';
-import logoImage from './logo/logo.jpg'
+import './Navigation.css'; // Import default styles
+import './dark-mode.css'; // Import dark mode styles
+import logoImage from './logo/logo.jpg';
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+    const sessionUser = useSelector(state => state.session.user);
+    const [darkMode, setDarkMode] = useState(false);
 
-	return (
-		<div className='nav-bar'>
-			<div>
-				<NavLink exact to="/">
-					<img src={logoImage} alt='Home' />
-				</NavLink>
-			</div>
-			{isLoaded && (
-				<div className='profile-button'>
-					<ProfileButton user={sessionUser} />
-				</div>
-			)}
-		</div>
-	);
+    const toggleDarkMode = () => {
+        window.alert('Feature coming soon!')
+        setDarkMode(prevDarkMode => !prevDarkMode);
+        console.log('this is dark mode', darkMode)
+    };
+
+    return (
+        <div className={`nav-bar`}>
+            <div className={`background-container ${darkMode ? 'dark-mode' : ''}`}>
+                <NavLink exact to="/">
+                    <img src={logoImage} alt='Home' />
+                </NavLink>
+            </div>
+            <div className='dark-mode-button'>
+                <button onClick={toggleDarkMode}>
+                    {darkMode ? 'Light Mode' : 'Dark Mode'}
+                </button>
+            </div>
+            {isLoaded && (
+                <div className='profile-button'>
+                    <ProfileButton user={sessionUser} />
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Navigation;
